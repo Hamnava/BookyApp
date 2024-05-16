b32454bdb5a0f289043154cda7745cff5631b4e9
Nematullah Hussaini <nematullahhussaini626l@gmail.com>
2024-05-16 09:09:14 +0430

    Availability service

--- WorkerService/Program.cs
+++ WorkerService/Program.cs
@@ -1,9 +1,11 @@
 using WorkerService;
+using WorkerService.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddHostedService<Worker>();
 builder.Services.AddHostedService<SecondWorker>();
+builder.Services.AddHostedService<AvailabilityService>();
 builder.Services.AddHostedService<SignalRWorker>();
 
 // Register HeartbeatSender as a hosted service
